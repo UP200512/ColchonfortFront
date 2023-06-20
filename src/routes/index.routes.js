a5e6205320002dee737b590bb6ed86b32092002e
up200512 <up200512@alumnos.upa.edu.mx>
2023-06-20 09:15:17 -0600

    junte lo de daniel y lo de pablo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,11 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "../views/Home/HomePage.jsx";
 import  Carta  from "../views/Carta/Carta.jsx";
 import PedidosPage from "../views/Pedidos/PedidosPage.jsx";
-<<<<<<< HEAD
-import PedidosNuevo from "../views/Pedidos/PedidosNuevos.jsx";
-=======
 import InsumosPage from "../views/Insumos/InsumosPage.jsx";
->>>>>>> Danielrescate
 
 
 function Autho() {
@@ -14,10 +10,7 @@ function Autho() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>}></Route>
-<<<<<<< HEAD
-=======
         <Route path="/insumos" element={<InsumosPage/>}></Route>
->>>>>>> Danielrescate
         <Route path="/Carta" element={<Carta/>}></Route>
         <Route path="/Pedidos" element={<PedidosPage/>}></Route>
         <Route path="/Pedidos/nuevo" element={<PedidosNuevo/>}></Route>
